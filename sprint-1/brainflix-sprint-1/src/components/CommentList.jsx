70f47058040c73722aa7c8f750587dd4d2a8f3d2
felixf3600 <felixf3600@gmail.com>
2020-08-09 21:24:57 -0700

    aded the digging deeper

--- sprint-1/brainflix-sprint-1/src/components/CommentList.jsx
+++ sprint-1/brainflix-sprint-1/src/components/CommentList.jsx
@@ -1,5 +1,36 @@
 import React from "react";
 
+function proccessedDate(commentTime) {
+  const now = new Date();
+  const old = new Date(commentTime);
+  const timeDifference = (now - old) / 1000;
+  const timeFrame = {
+    minute: 60,
+    hour: 3600,
+    day: 86400,
+    week: 604800,
+    month: 259200,
+    year: 31536000,
+  };
+  let returnTime = "";
+  if (timeDifference < timeFrame.minute) {
+    returnTime = `${Math.ceil(timeDifference)} sec ago`;
+  } else if (timeDifference < timeFrame.hour) {
+    returnTime = `${Math.floor(timeDifference / timeFrame.minute)} min ago`;
+  } else if (timeDifference < timeFrame.day) {
+    returnTime = `${Math.floor(timeDifference / timeFrame.hour)} hours ago`;
+  } else if (timeDifference < timeFrame.week) {
+    returnTime = `${Math.floor(timeDifference / timeFrame.day)} days ago`;
+  } else if (timeDifference < timeFrame.month) {
+    returnTime = `${Math.floor(timeDifference / timeFrame.week)} week ago`;
+  } else if (timeDifference < timeFrame.year) {
+    returnTime = `${Math.floor(timeDifference / timeFrame.month)} months ago`;
+  } else {
+    returnTime = `${Math.floor(timeDifference / timeFrame.year)} year ago`;
+  }
+  return returnTime;
+}
+
 function CommentList(props) {
   const classes = [
     "comments__image",
@@ -9,6 +40,7 @@ function CommentList(props) {
     "comments__left-side",
     "comments__rigth-side",
   ];
+
   return (
     <ul className={props.classes}>
       {props.commentList.map((comment) => (
@@ -18,7 +50,7 @@ function CommentList(props) {
           </div>
           <div className={classes[5]}>
             <p className={classes[1]}>{comment.name}</p>
-            <p className={classes[2]}>{comment.timeStamp}</p>
+            <p className={classes[2]}>{proccessedDate(comment.timeStamp)}</p>
             <p className={classes[3]}>{comment.comment}</p>
           </div>
         </li>
